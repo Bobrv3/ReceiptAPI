8c963a67a46ad5151a9478aa6e240d457a09fbe5
Anton Bobrov <anton.bobrov.98@gmail.com>
2022-12-15 16:57:48 +0300

    chore: init sql scripts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,46 @@
+INSERT
+INTO
+  discount_cards
+  (id, discount_size)
+VALUES
+  (nextval('discount_card_seq'), 5),
+  (nextval('discount_card_seq'), 3),
+  (nextval('discount_card_seq'), 6),
+  (nextval('discount_card_seq'), 2);
+
+INSERT
+INTO
+  products
+  (id, description, price)
+VALUES
+  (nextval('product_seq'), 'Milk', 1.6),
+  (nextval('product_seq'), 'Bread', 1.45),
+  (nextval('product_seq'), 'Meat', 7.5),
+  (nextval('product_seq'), 'Apple', 2),
+  (nextval('product_seq'), 'Banana', 2.45);
+
+INSERT
+INTO
+  orders
+  (id, discount_card_id)
+VALUES
+  (nextval('order_seq'), 1),
+  (nextval('order_seq'), 2),
+  (nextval('order_seq'), 3),
+  (nextval('order_seq'), 4),
+  (nextval('order_seq'), null);
+
+  INSERT
+INTO
+  order_items
+  (quantity, order_id, product_id)
+VALUES
+  (1, 1, 1),
+  (6, 1, 4),
+  (1, 2, 2),
+  (1, 2, 3),
+  (7, 3, 5),
+  (1, 4, 1),
+  (1, 4, 2),
+  (1, 4, 5),
+  (10, 5, 4);
